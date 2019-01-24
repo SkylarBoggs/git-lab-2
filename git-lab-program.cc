2acb986c97d253cf846b82b804cc180efb1f9237
Skylar Boggs <sb667617@ohio.edu>
2019-01-23 19:27:14 -0500

    Updated program with name, date, and description

---
+++ git-lab-program.cc
@@ -0,0 +1,19 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: Skylar Boggs
+ *        Date: 1/23/19
+ * Description: Program for Lab 2, hello git
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    cout << "Hello Git!!" << endl;
+    return 0;
+
+}// main
